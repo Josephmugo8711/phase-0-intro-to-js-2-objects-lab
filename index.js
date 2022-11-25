5a399ac459e812a7362a061b230cb055df183c8f
Joseph <wamiti8711@gmail.com>
2022-11-25 17:21:23 -0500

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution in this file!
+const employee = {};
+employee.name = "Joseph";
+employee.streetAddress = "Nanyuki";
+
+
+employee;
+
+//it does not modify the original employee, 
+//but rather returns a clone with the new data
+//copy the contents from the employee data
+// modifying objects
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value
+
+    return newEmployee
+}
+
+const nonDestructiveUpdatedEmployee = updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "sam"
+    )
+
+    //adding object using dot notation
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    
+    return employee;
+}
+
+const destructiveEmployeeUpdates = destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "sam"
+   )
+   console.log(destructiveEmployeeUpdates)
+
+function deleteFromEmployeeByKey(employee,key){
+    const employeeCopy = {...employee}
+    delete employeeCopy["name"]
+
+
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    const newEmployee = delete employee["name"]
+
+    return employee;
+}
+
